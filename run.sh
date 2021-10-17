434c0ab6bfc841cbb72ab842c7d35a375aa82c43
Pu Wang <nicolas.weeks@gmail.com>
2021-10-17 13:51:59 +0800

    enable format and lint check

---
+++ run.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+#set -o errexit # TODO: enable this check
+set -o nounset
+set -o xtrace
+
+docker pull ghcr.io/chipsalliance/verible-linter-action
+docker pull 0x01be/iverilog
+
+FILE_LIST=`find ./src -name "*v"`
+# Format check
+docker run --rm -v `pwd`:`pwd` -w `pwd` --entrypoint verible-verilog-format ghcr.io/chipsalliance/verible-linter-action --failsafe_success=false --inplace --verbose $FILE_LIST
+# Lint check
+docker run --rm -v `pwd`:`pwd` -w `pwd` --entrypoint verible-verilog-lint ghcr.io/chipsalliance/verible-linter-action --ruleset=all --show_diagnostic_context $FILE_LIST
+
+BUILD_DIR=build
+SIM_ELF=a.out
+SIM_ELF_PATH=$BUILD_DIR/$SIM_ELF
+mkdir -p $BUILD_DIR
+docker run --rm -v `pwd`:`pwd` -w `pwd` -u root 0x01be/iverilog iverilog -g2012 -o $SIM_ELF_PATH -I ./src ./src/testbench.v
+cd $BUILD_DIR
+docker run --rm -v `pwd`:`pwd` -w `pwd` -u root 0x01be/iverilog ./$SIM_ELF
+
